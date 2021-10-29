563e7b691afdf2b89058950c3db3a7b73294afbc
Adithya-Jayan <adithyaj8@gmail.com>
2021-10-29 19:49:28 +0530

    Starts with genetic learning algorithm

---
+++ Image_processing_Unity_Project/Assets/Scripts/NN_Brain.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NN_Brain : MonoBehaviour
+{
+    public GameObject LowerLeg_L;
+    public GameObject LowerLeg_R;
+    public GameObject UpperLeg_L;
+    public GameObject UpperLeg_R;
+    public GameObject Torso;
+
+    int[] Weights_shape = {30, 25, 20, 16, 8, 4 };
+
+    float[,] Weights;
+
+    float[] Read_inputs()
+    {
+        List<float> Input = new List<float>();
+
+        Input.Add(LowerLeg_L.GetComponent<Rigidbody>().angularVelocity.x);
+        Input.Add(LowerLeg_L.GetComponent<Rigidbody>().angularVelocity.y);
+        Input.Add(LowerLeg_L.GetComponent<Rigidbody>().angularVelocity.z);
+
+        Input.Add(LowerLeg_R.GetComponent<Rigidbody>().angularVelocity.x);
+        Input.Add(LowerLeg_R.GetComponent<Rigidbody>().angularVelocity.y);
+        Input.Add(LowerLeg_R.GetComponent<Rigidbody>().angularVelocity.z);
+
+        Input.Add(UpperLeg_L.GetComponent<Rigidbody>().angularVelocity.x);
+        Input.Add(UpperLeg_L.GetComponent<Rigidbody>().angularVelocity.y);
+        Input.Add(UpperLeg_L.GetComponent<Rigidbody>().angularVelocity.z);
+
+        Input.Add(UpperLeg_R.GetComponent<Rigidbody>().angularVelocity.x);
+        Input.Add(UpperLeg_R.GetComponent<Rigidbody>().angularVelocity.y);
+        Input.Add(UpperLeg_R.GetComponent<Rigidbody>().angularVelocity.z);
+
+        Input.Add(Torso.GetComponent<Rigidbody>().angularVelocity.x);
+        Input.Add(Torso.GetComponent<Rigidbody>().angularVelocity.y);
+        Input.Add(Torso.GetComponent<Rigidbody>().angularVelocity.z);
+
+        Input.Add(LowerLeg_L.GetComponent<Rigidbody>().velocity.x);
+        Input.Add(LowerLeg_L.GetComponent<Rigidbody>().velocity.y);
+        Input.Add(LowerLeg_L.GetComponent<Rigidbody>().velocity.z);
+
+        Input.Add(LowerLeg_R.GetComponent<Rigidbody>().velocity.x);
+        Input.Add(LowerLeg_R.GetComponent<Rigidbody>().velocity.y);
+        Input.Add(LowerLeg_R.GetComponent<Rigidbody>().velocity.z);
+
+        Input.Add(UpperLeg_L.GetComponent<Rigidbody>().velocity.x);
+        Input.Add(UpperLeg_L.GetComponent<Rigidbody>().velocity.y);
+        Input.Add(UpperLeg_L.GetComponent<Rigidbody>().velocity.z);
+
+        Input.Add(UpperLeg_R.GetComponent<Rigidbody>().velocity.x);
+        Input.Add(UpperLeg_R.GetComponent<Rigidbody>().velocity.y);
+        Input.Add(UpperLeg_R.GetComponent<Rigidbody>().velocity.z);
+
+        Input.Add(Torso.GetComponent<Rigidbody>().velocity.x);
+        Input.Add(Torso.GetComponent<Rigidbody>().velocity.y);
+        Input.Add(Torso.GetComponent<Rigidbody>().velocity.z);
+
+        return Input.ToArray();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float[] Inputs = Read_inputs();
+
+        float[] Layer_input = Inputs;
+        float[] Layer_output;
+   
+        //Propogate Inputs through layers
+        for (int i = 1; i < Weights_shape.Length; i++)
+        {
+            for (int j = 0; j < Weights_shape[i-1]; j++)
+            {
+                for (int p = 0; p < Layer_input.Length; p++)
+                {
+                    
+                }
+            }
+        }
+
+    }
+}
--- Image_processing_Unity_Project/Assets/Scripts/Walker_AI.cs
+++ Image_processing_Unity_Project/Assets/Scripts/Walker_AI.cs
@@ -10,6 +10,9 @@ public class Walker_AI : MonoBehaviour
     public GameObject UpperLeg_L;
     public GameObject UpperLeg_R;
 
+    
+    public float Target_Velocity;
+
     private struct Limb 
     {
         public Vector3 velocity;
@@ -26,30 +29,34 @@ public class Walker_AI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        LowerLeg_L.GetComponent<Rigidbody>().maxAngularVelocity = Mathf.Infinity;
+        LowerLeg_R.GetComponent<Rigidbody>().maxAngularVelocity = Mathf.Infinity;
+        UpperLeg_L.GetComponent<Rigidbody>().maxAngularVelocity = Mathf.Infinity;
+        UpperLeg_R.GetComponent<Rigidbody>().maxAngularVelocity = Mathf.Infinity;
         i = 1;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        
         var LowerLeg_L_motor = LowerLeg_L.GetComponent<HingeJoint>().motor;
         var LowerLeg_R_motor = LowerLeg_R.GetComponent<HingeJoint>().motor;
         var UpperLeg_L_motor = UpperLeg_L.GetComponent<HingeJoint>().motor;
         var UpperLeg_R_motor = UpperLeg_R.GetComponent<HingeJoint>().motor;
 
         Lower_L.velocity  = LowerLeg_L.GetComponent<Rigidbody>().angularVelocity;
-        Debug.Log(Lower_L.velocity);
         
         if(Mathf.Round(Time.time)%10 == 0 & (Time.time - currentTime > 1))
         {
-            Debug.Log(LowerLeg_L_motor.targetVelocity);
-
-            LowerLeg_L_motor.targetVelocity = 15*i;
-            LowerLeg_L.GetComponent<HingeJoint>().motor = LowerLeg_L_motor;
+            LowerLeg_L_motor.targetVelocity = Target_Velocity * i;
             i = -1 * i;
             currentTime = Time.time;
         }
-            
+
+        LowerLeg_L.GetComponent<HingeJoint>().motor = LowerLeg_L_motor;
+        Debug.Log(LowerLeg_L_motor.targetVelocity);
+
 
     }
 }
